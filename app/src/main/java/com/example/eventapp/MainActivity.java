96c152fb44bcb983a9ecbf1a70515086e862c125
yashnahta <yashnahtanaman12345@gmail.com>
2019-02-07 19:27:13 +0530

    commit1

--- app/src/main/java/com/example/eventapp/MainActivity.java
+++ app/src/main/java/com/example/eventapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.eventapp;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
